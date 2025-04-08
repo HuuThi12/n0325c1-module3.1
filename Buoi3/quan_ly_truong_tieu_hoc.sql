211cc70483531a30e7728689adc940abaac58932
HuuThi12 <huuthi12072003@gmail.com>
2025-04-08 16:47:25 +0700

    Exercise_day3

--- Buoi3/quan_ly_truong_tieu_hoc.sql
+++ Buoi3/quan_ly_truong_tieu_hoc.sql
@@ -2,13 +2,13 @@ create database quan_ly_truong_tieu_hoc;
 use quan_ly_truong_tieu_hoc;
 drop database quan_ly_truong_tieu_hoc;
 
--- 1
+
 create table giao_vien(
 	ma_gv char(5) primary key,
     ho_ten_gv varchar(255) 
 );
 
--- 2
+
 create table lop(
 	ma_lop char(5) primary key,
     ten_lop varchar(255) ,
@@ -17,7 +17,7 @@ create table lop(
     foreign key (ma_gvcn) references giao_vien(ma_gv)
 );
 
--- 3
+
 create table hoc_sinh(
 	ma_hs char(5)  primary key,
     ho_ten_hs varchar(255) ,
@@ -28,7 +28,7 @@ create table hoc_sinh(
     foreign key (ma_lop) references lop(ma_lop)
 );
 
--- 4
+
 create table mon_hoc(
 	ma_mh char(5)  primary key,
     ten_mh varchar(255) 
@@ -36,7 +36,7 @@ create table mon_hoc(
 
 
 
--- 5
+
 create table ket_qua_hoc_tap(
 	hoc_ky varchar(10) ,
 	ma_hs char(5) ,
@@ -60,12 +60,128 @@ create table phu_trach_bo_mon(
     foreign key (ma_mh) references mon_hoc(ma_mh)
 );
 
--- bài 4 Luyện tập về không Where
--- câu a
+-- Câu 1: liệt kê thông tin giáo viên, học sinh và lớp học trong trường
+-- 1a. Toàn bộ thông tin của toàn bộ giáo viên trong trường
+select * from giao_vien;
+-- 1b. Họ tên học sinh, giới tính, họ tên phụ huynh của toàn bộ học sinh trong trường
+select ho_ten_hs , ho_ten_ph , gioi_tinh from hoc_sinh;
+-- 1c.Toàn bộ thông tin của tất cả các lớp trong trường
+select * from lop;
+
+-- Câu 2: Truy vấn dữ liệu học sinh và lớp học
+-- 2a: Những học sinh có giới tính là nam
+select * from hoc_sinh;
 select * from hoc_sinh where gioi_tinh = 'nam';
 
--- câu b
-select * from hoc_sinh where ho_ten_ph is null;
+-- 2b 
+select ho_ten_hs, gioi_tinh, dia_chi from hoc_sinh where ho_ten_ph is null;
+
+-- 2c: Những lớp chưa có giáo viên chủ nhiệm
+select * from lop where ma_gvcn IS NULL;
+
+-- 2d: Những học sinh chưa được phân lớp
+select * from hoc_sinh where ma_lop is null ;
+
+-- 2e: Những học sinh nữ có địa chỉ ở thanh khuê
+select * from hoc_sinh where gioi_tinh = 'Nữ' and dia_chi like '%Thanh Khê%' ;
+
+-- 2f. Những học sinh nam có địa chỉ ở Hải Châu hoặc những học sinh nữ có địa chỉ ở Thanh Khê.
+select * from hoc_sinh where (gioi_tinh = 'nam' and dia_chi like '%Hải Châu%') or (gioi_tinh = 'nữ' and dia_chi like '%Thanh Khê%');
+
+-- 2g. Những học sinh nam chưa có tên phụ huynh và những học sinh nữ chưa được phân lớp.
+select * from hoc_sinh where (gioi_tinh = 'Nam' and ho_ten_ph is null) and (gioi_tinh = 'Nữ' and ma_lop is null);
+
+-- 2h. Những học sinh nam chưa được phân lớp và những học sinh nam chưa có tên phụ huynh.
+select * from hoc_sinh where gioi_tinh = 'Nam' and  (ma_lop is null or ho_ten_ph is null);
+
+-- 2i. Mã môn học của những môn học được dạy trong học kỳ 1.
+select * from ket_qua_hoc_tap;
+select distinct ma_mh from phu_trach_bo_mon where hoc_ky = 'Học Kỳ 1';
+
+
+
+-- câu 3 Like
+-- a. Những học sinh có họ bắt đầu bằng từ Nguyễn.
+select * from hoc_sinh where ho_ten_hs like "Nguyễn%";
+
+-- b. Những học sinh có họ kết thúc bằng từ Nở.
+SELECT * FROM hoc_sinh WHERE  ho_ten_hs LIKE  "%Nở";
+
+-- c. Những học sinh có họ tên chửa từ Thị.
+select * from hoc_sinh where ho_ten_hs like "%Thị%";
+
+-- d. Những học sinh chứa từ Thị ở giữa (không được chửa ở đầu và ở cuối).
+select * from hoc_sinh where ho_ten_hs like "%Thị%";
+
+-- e. Những học sinh có họ tên với độ dài là 30 ký tự (kể cả khoảng trắng).
+-- không được
+SELECT * FROM hoc_sinh WHERE length(ho_ten_hs) = 30;
+
+-- f. Những học sinh có họ tên với độ dài tối đa là 30 ký tự.
+SELECT * FROM hoc_sinh WHERE length(ho_ten_hs) <= 30;
+
+-- g. Những học sinh có họ tên với độ dài tối đa 30 ký tự và bắt đầu bằng ký tự N.
+SELECT * FROM hoc_sinh WHERE length(ho_ten_hs) <= 30 and ho_ten_hs like'N%';
+
+-- h. Những học sinh có họ tên bắt đầu bằng các ký tự: N, T, V.
+SELECT * FROM hoc_sinh WHERE ho_ten_hs like 'N%' or ho_ten_hs like 'T%' or ho_ten_hs like 'V%' ;
+
+-- i. Những học sinh có họ tên không được bắt đầu bằng các ký tự: N, T, V.
+SELECT * FROM hoc_sinh WHERE ho_ten_hs not like 'N%' and ho_ten_hs not like 'T%' and  ho_ten_hs not like 'V%' ;
+
+-- j. Những học sinh có họ tên với phần họ có đúng 4 ký tự.
+
+
+
+-- Câu 4: Luyện tập về DISTINCT
+-- a. Họ tên của toàn bộ học sinh trong trường, nếu họ tên nào trùng nhau thì chỉ hiển thị 1 lần.
+select distinct ho_ten_hs from hoc_sinh ;
+
+-- b. Mã lớp của các lớp đã có học sinh học ở lớp đó. Nếu mã lớp nào trùng nhau thì chỉ hiển thị 1 lần.
+select distinct ma_lop from hoc_sinh ;
+
+-- c. Mã môn học của những môn học đã có ít nhất 1 giáo viên được phân công phụ trách rồi. (*)
+-- không được
+select distinct ma_gvpt from phu_trach_bo_mon where ma_mh is null;
+
+-- d. Mã môn học của những môn học đã từng được tổ chức thi ít nhất 1 lần.
+select distinct ma_mh from ket_qua_hoc_tap ;
+
+-- e. Mã giáo viên của những giáo viên đã từng làm chủ nhiệm cho ít nhất 1 lớp nào đó.
+select distinct ma_gvcn from lop ;
+
+
+-- Câu 5: Luyện tập về ORDER BY
+-- asc : tăng dần ( mặc định )
+-- desc: giảm dần
+
+-- a. Thông tin của toàn bộ học sinh trong trường. Kết quả trả về cần được sắp xếp tăng dần theo tên học sinh.
+select * from hoc_sinh ORDER BY ho_ten_hs ; 
+
+-- b. Thông tin của toàn bộ học sinh trong trường. Kết quả trả về cần được sắp xếp giảm dần theo địa chỉ.
+select * from hoc_sinh ORDER BY dia_chi desc;
+
+-- c. Thông tin của toàn bộ học sinh trong trường. Kết quả trả về cần được sắp xếp tăng dần theo tên học sinh và giảm dần theo địa chỉ.
+select * from hoc_sinh ORDER BY ho_ten_hs asc, dia_chi desc;
+
+-- d. Thông tin của toàn bộ học sinh trong trường. Kết quả trả về cần được sắp xếp tăng dần theo tên học sinh và tăng dần theo địa chỉ.
+select * from hoc_sinh ORDER BY ho_ten_hs , dia_chi ;
+
+-- e. Thông tin của toàn bộ học sinh trong trường. Kết quả trả về cần được sắp xếp giảm dần theo tên học sinh, giảm dần theo địa chỉ.
+select * from hoc_sinh ORDER BY ho_ten_hs desc , dia_chi desc;
+
+-- f. Thông tin của toàn bộ học sinh trong trường. Kết quả trả về cần được sắp xếp giảm dần theo tên học sinh, giảm dần theo địa chỉ và tăng dần theo tên phụ huynh.
+select * from hoc_sinh ORDER BY ho_ten_hs desc , dia_chi desc , ho_ten_ph asc;
+
+-- Câu 6: Luyện tập về JOIN 2 bảng
+-- a. Liệt kê ma_hoc_sinh, ho_ten_hoc_sinh, gioi_tinh, ma_lop, ten_lop, ma_gvcn của tất cả học sinh trong trường.
+-- b. Liệt kê ma_hoc_sinh, ho_ten_hoc_sinh, hoc_ky, ma_mon_hoc, diem_thi_giua_ky, diem_thi_cuoi_ky của tất cả học sinh và các môn học có kết quả tương ứng với từng học sinh trong trường.
+-- c. Liệt kê ma_giao_vien, ho_ten_giao_vien, ma_lop, ma_mon_hoc, hoc_ky của những giáo viên đã được phân công phụ trách ít nhất một môn học.
+-- d. Suy nghĩ về yêu cầu a ở trên: Nếu học sinh chưa được phân lớp thì liệu có liệt kê được học sinh đó không?
+-- e. Suy nghĩ về yêu cầu b ở trên: Nếu học sinh chưa có kết quả thi của môn nào cả thì liệu có liệt kê được học sinh đó không?
+-- f. Suy nghĩ về yêu cầu c ở trên: Nếu giáo viên chưa phụ trách một môn nào cả thì liệu có liệt kê được giáo viên đó không?
+
+
 
 
 
