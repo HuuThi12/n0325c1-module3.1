fb0bad000a23eb75e3ca7f32323d6ce5b8aafeed
HuuThi12 <huuthi12072003@gmail.com>
2025-04-08 17:04:21 +0700

    Exercise day 3 update

--- Buoi3/quan_ly_truong_tieu_hoc.sql
+++ Buoi3/quan_ly_truong_tieu_hoc.sql
@@ -178,8 +178,17 @@ select * from hoc_sinh ORDER BY ho_ten_hs desc , dia_chi desc , ho_ten_ph asc;
 select hs.ma_hs, hs.ho_ten_hs, hs.gioi_tinh, hs.ma_lop, lop.ten_lop, lop.ma_gvcn
 from hoc_sinh hs
 inner join lop on hs.ma_lop = lop.ma_lop;
+
 -- b. Liệt kê ma_hoc_sinh, ho_ten_hoc_sinh, hoc_ky, ma_mon_hoc, diem_thi_giua_ky, diem_thi_cuoi_ky của tất cả học sinh và các môn học có kết quả tương ứng với từng học sinh trong trường.
+select hs.ma_hs, hs.ho_ten_hs, kq.hoc_ky, kq.ma_mh, kq.diem_thi_giua_ky, kq.diem_thi_cuoi_ky
+from hoc_sinh hs
+inner join  ket_qua_hoc_tap kq on hs.ma_hs = kq.ma_hs;
+
 -- c. Liệt kê ma_giao_vien, ho_ten_giao_vien, ma_lop, ma_mon_hoc, hoc_ky của những giáo viên đã được phân công phụ trách ít nhất một môn học.
+select gv.ma_gv, gv.ho_ten_gv, pt.ma_lop, pt.ma_mh, pt.hoc_ky
+from giao_vien gv
+inner join  phu_trach_bo_mon pt on gv.ma_gv = pt.ma_gvpt;
+
 -- d. Suy nghĩ về yêu cầu a ở trên: Nếu học sinh chưa được phân lớp thì liệu có liệt kê được học sinh đó không?
 -- e. Suy nghĩ về yêu cầu b ở trên: Nếu học sinh chưa có kết quả thi của môn nào cả thì liệu có liệt kê được học sinh đó không?
 -- f. Suy nghĩ về yêu cầu c ở trên: Nếu giáo viên chưa phụ trách một môn nào cả thì liệu có liệt kê được giáo viên đó không?
